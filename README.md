d0f9d6ab662c3aa8a00d933d9dddaa89a83042cc
Prasad Tengse <tprasadtp@users.noreply.github.com>
2021-06-05 16:41:22 +0200

    docs: Update README

--- README.md
+++ README.md
@@ -1,16 +1,8 @@
-<!-- markdownlint-disable MD033 -->
-
 # VirtIO WHQL Drivers
 
 [![build](https://github.com/tprasadtp/virtio-whql/actions/workflows/build.yml/badge.svg)](https://github.com/tprasadtp/virtio-whql/actions/workflows/build.yml)
-[![releases](https://img.shields.io/github/v/tag/tprasadtp/virtio-whql?label=version&sort=semver&logo=semver&color=7f50a6&labelColor=3a3a3a)](https://github.com/tprasadtp/virtio-whql/releases/latest)
+[![releases](https://img.shields.io/github/v/tag/tprasadtp/virtio-whql?label=release&sort=semver&logo=semver&color=7f50a6&labelColor=3a3a3a)](https://github.com/tprasadtp/virtio-whql/releases/latest)
 ![analytics](https://ga-beacon.prasadt.com/UA-101760811-3/github/virtio-whql)
-
 
 - `virtio` drivers for Windows.
 - These are signed by Microsoft and are sourced from CentOS stream repository
-- Kept updated by `repology+renovatebot+GitHub-Actions`
-
-<!-- CI Badges -->
-
-[releases]: https://github.com/tprasadtp/virtio-whql/releases/latest
--- tools/release.sh
+++ tools/release.sh
@@ -2,27 +2,28 @@
 
 set -euo pipefail
 
-if [[ ! -e docker/build/VERSION.txt ]]; then
-    echo "Error! VERSION.txt not found!"
+if [[ ! -f docker/build/VERSION.txt ]]; then
+    echo "Error! VERSION.txt file not found!"
     exit 1
 fi
 
 CURRENT_VERSION=$(cat docker/build/VERSION.txt | tr -dc '[:print:]')
 
 if [[ -z $CURRENT_VERSION ]]; then
-    echo "Error! Current VERSION.txt file is empty!"
+    echo "Error! VERSION.txt file is empty!"
     exit 1
 fi
 
 LATEST_RELEASE="$(gh api repos/:owner/:repo/releases/latest --jq '.tag_name')"
 
 if [[ -z $LATEST_RELEASE ]]; then
-    echo "Error! Failed to get current latest tag!"
+    echo "Error! Failed to get latest tag from GitHub!"
     exit 1
 fi
 
 if [[ $CURRENT_VERSION == "$LATEST_RELEASE" ]]; then
-    echo "All Good! Latest reease is same as extracted!"
+    echo "All Good! No need to create a new release"
+    echo "Latest release version is same as extracted!"
 else
     echo "Current Version: $CURRENT_VERSION"
     echo "Latest Release: $LATEST_RELEASE"
